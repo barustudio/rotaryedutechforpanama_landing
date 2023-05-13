18fbdb94924d5a9e6d2e9195e2311f1aa67a29f5
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2023-05-12 20:57:44 -0500

    refactor: migrate the preloader and back to top button

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,17 @@
 <script>
   import About from "./About.svelte";
-    import CallToAction from "./CallToAction.svelte";
-    import Contact from "./Contact.svelte";
+  import CallToAction from "./CallToAction.svelte";
+  import Contact from "./Contact.svelte";
   import Header from "./Header.svelte";
   import News from "./News.svelte";
-    import Partners from "./Partners.svelte";
-    import Testimonials from "./Testimonials.svelte";
+  import Partners from "./Partners.svelte";
+  import Preloader from "./Preloader.svelte";
+  import Testimonials from "./Testimonials.svelte";
+  import BackToTopButton from "./lib/BackToTopButton.svelte";
 </script>
 
 <div>
+  <Preloader />
   <Header />
   <About />
   <News />
@@ -16,6 +19,7 @@
   <CallToAction />
   <Testimonials />
   <Contact />
+  <BackToTopButton />
 </div>
 
 <style></style>
