de68932ea4420bf7408e2dec3db9789ee0704ba2
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2023-05-12 22:29:37 -0500

    refactor(News): migrate the data to data.json and use the data as variables in Svelte component

--- src/News.svelte
+++ src/News.svelte
@@ -1,157 +1,54 @@
-<script></script>
+<script>
+    import data from './data.json';
+    let { sectionTitle, sectionSubtitle, categories,    items } = data.news;
+  </script>
 
-<section id="news" class="news-area">
-  <div class="container">
+  <section id="news" class="news-area">
+    <div class="container">
       <div class="row justify-content-center">
-          <div class="col-lg-6">
-              <div class="section-title text-center pb-20">
-                  <h3 class="title">News & Events</h3>
-                  <p class="text">Upcoming events and recent news articles about the project</p>
-              </div> <!-- row -->
+        <div class="col-lg-6">
+          <div class="section-title text-center pb-20">
+            <h3 class="title">{sectionTitle}</h3>
+            <p class="text">{sectionSubtitle}</p>
           </div>
-      </div> <!-- row -->
+        </div>
+      </div>
       <div class="row">
-          <div class="col-lg-12">
-              <div class="news-menu pt-30 text-center">
-                  <ul>
-                      <li data-filter="*" class="active">ALL</li>
-                      <li data-filter=".branding-3">BRANDING</li>
-                      <li data-filter=".marketing-3">MARKETING</li>
-                      <li data-filter=".planning-3">PLANNING</li>
-                      <li data-filter=".research-3">RESEARCH</li>
-                  </ul>
-              </div> <!-- news menu -->
+        <div class="col-lg-12">
+          <div class="news-menu pt-30 text-center">
+            <ul>
+                <li data-filter="*" class="active">ALL</li>
+                {#each categories as category}
+                <li data-filter=".{category.toLowerCase()}-3">{category}</li>
+                {/each}
+            </ul>
           </div>
-      </div> <!-- row -->
+        </div>
+      </div>
       <div class="row grid">
-          <div class="col-lg-4 col-sm-6 branding-3 planning-3">
-              <div class="single-news mt-30 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="0.2s">
-                  <div class="news-image">
-                      <img src="assets/images/news-1.png" alt="">
-                      <div class="news-overlay d-flex align-items-center justify-content-center">
-                          <div class="news-content">
-                              <div class="news-icon">
-                                  <a class="image-popup" href="assets/images/news-1.png"><i class="lni-zoom-in"></i></a>
-                              </div>
-                              <div class="news-icon">
-                                  <a href="#"><i class="lni-link"></i></a>
-                              </div>
-                          </div>
-                      </div>
+        {#each items as item (item.id)}
+          <div class="col-lg-4 col-sm-6 {item.categories.join(' ')}">
+            <div class="single-news mt-30 wow fadeInUp" data-wow-duration="1.5s">
+            <div class="news-image">
+              <img src="{item.image}" alt="">
+              <div class="news-overlay d-flex align-items-center justify-content-center">
+                <div class="news-content">
+                  <div class="news-icon">
+                    <a class="image-popup" href="{item.image}"><i class="lni-zoom-in"></i></a>
                   </div>
-                  <div class="news-text">
-                      <h4 class="news-title"><a href="#">Graphics Design</a></h4>
-                      <p class="text">Short description for the ones who look for something new. Awesome!</p>
+                  <div class="news-icon">
+                    <a href="#"><i class="lni-link"></i></a>
                   </div>
-              </div> <!-- single news -->
+                </div>
+              </div>
+            </div>
+            <div class="news-text">
+              <h4 class="news-title"><a href="#">{item.title}</a></h4>
+              <p class="text">{item.description}</p>
+            </div>
           </div>
-          <div class="col-lg-4 col-sm-6 marketing-3 research-3">
-              <div class="single-news mt-30 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="0.4s">
-                  <div class="news-image">
-                      <img src="assets/images/news-2.png" alt="">
-                      <div class="news-overlay d-flex align-items-center justify-content-center">
-                          <div class="news-content">
-                              <div class="news-icon">
-                                  <a class="image-popup" href="assets/images/news-2.png"><i class="lni-zoom-in"></i></a>
-                              </div>
-                              <div class="news-icon">
-                                  <a href="#"><i class="lni-link"></i></a>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="news-text">
-                      <h4 class="news-title"><a href="#">Graphics Design</a></h4>
-                      <p class="text">Short description for the ones who look for something new. Awesome!</p>
-                  </div>
-              </div> <!-- single news -->
-          </div>
-          <div class="col-lg-4 col-sm-6 branding-3 marketing-3">
-              <div class="single-news mt-30 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="0.7s">
-                  <div class="news-image">
-                      <img src="assets/images/news-3.png" alt="">
-                      <div class="news-overlay d-flex align-items-center justify-content-center">
-                          <div class="news-content">
-                              <div class="news-icon">
-                                  <a class="image-popup" href="assets/images/news-3.png"><i class="lni-zoom-in"></i></a>
-                              </div>
-                              <div class="news-icon">
-                                  <a href="#"><i class="lni-link"></i></a>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="news-text">
-                      <h4 class="news-title"><a href="#">Graphics Design</a></h4>
-                      <p class="text">Short description for the ones who look for something new. Awesome!</p>
-                  </div>
-              </div> <!-- single news -->
-          </div>
-          <div class="col-lg-4 col-sm-6 planning-3 research-3">
-              <div class="single-news mt-30 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="0.2s">
-                  <div class="news-image">
-                      <img src="assets/images/news-4.png" alt="">
-                      <div class="news-overlay d-flex align-items-center justify-content-center">
-                          <div class="news-content">
-                              <div class="news-icon">
-                                  <a class="image-popup" href="assets/images/news-4.png"><i class="lni-zoom-in"></i></a>
-                              </div>
-                              <div class="news-icon">
-                                  <a href="#"><i class="lni-link"></i></a>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="news-text">
-                      <h4 class="news-title"><a href="#">Graphics Design</a></h4>
-                      <p class="text">Short description for the ones who look for something new. Awesome!</p>
-                  </div>
-              </div> <!-- single news -->
-          </div>
-          <div class="col-lg-4 col-sm-6 marketing-3">
-              <div class="single-news mt-30 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="0.4s">
-                  <div class="news-image">
-                      <img src="assets/images/news-5.png" alt="">
-                      <div class="news-overlay d-flex align-items-center justify-content-center">
-                          <div class="news-content">
-                              <div class="news-icon">
-                                  <a class="image-popup" href="assets/images/news-5.png"><i class="lni-zoom-in"></i></a>
-                              </div>
-                              <div class="news-icon">
-                                  <a href="#"><i class="lni-link"></i></a>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="news-text">
-                      <h4 class="news-title"><a href="#">Graphics Design</a></h4>
-                      <p class="text">Short description for the ones who look for something new. Awesome!</p>
-                  </div>
-              </div> <!-- single news -->
-          </div>
-          <div class="col-lg-4 col-sm-6 planning-3">
-              <div class="single-news mt-30 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="0.7s">
-                  <div class="news-image">
-                      <img src="assets/images/news-6.png" alt="">
-                      <div class="news-overlay d-flex align-items-center justify-content-center">
-                          <div class="news-content">
-                              <div class="news-icon">
-                                  <a class="image-popup" href="assets/images/news-6.png"><i class="lni-zoom-in"></i></a>
-                              </div>
-                              <div class="news-icon">
-                                  <a href="#"><i class="lni-link"></i></a>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="news-text">
-                      <h4 class="news-title"><a href="#">Graphics Design</a></h4>
-                      <p class="text">Short description for the ones who look for something new. Awesome!</p>
-                  </div>
-              </div> <!-- single news -->
-          </div>
-      </div> <!-- row -->
-  </div> <!-- container -->
+        </div>
+      {/each}
+    </div>
+  </div>
 </section>
-
-<style></style>
