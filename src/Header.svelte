ab819f89901fac02b01e8f7637f4ee0ff03bec86
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2023-05-12 22:17:40 -0500

    refactor(Header): migrate the data to data.json and use the data as variables in Svelte component

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,119 +1,117 @@
-<script></script>
+<script>
+    import data from "./data.json";
+    let { navbarItems, carouselItems } = data.header;
+</script>
 
 <section class="header-area">
-  <div class="navbar-area">
-      <div class="container">
-          <div class="row">
-              <div class="col-lg-12">
-                  <nav class="navbar navbar-expand-lg">
-                      <a class="navbar-brand" href="#">
-                          <img src="assets/images/logo.png" alt="Logo of Digital Interactive Classrooms Project, a Rotary initiative.">
-                      </a>
+    <div class="navbar-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-lg">
+                        <a class="navbar-brand" href="#">
+                            <img
+                                src="assets/images/logo.png"
+                                alt="Logo of Digital Interactive Classrooms Project, a Rotary initiative."
+                            />
+                        </a>
 
-                      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarEight" aria-controls="navbarEight" aria-expanded="false" aria-label="Toggle navigation">
-                          <span class="toggler-icon"></span>
-                          <span class="toggler-icon"></span>
-                          <span class="toggler-icon"></span>
-                      </button>
+                        <button
+                            class="navbar-toggler"
+                            type="button"
+                            data-toggle="collapse"
+                            data-target="#navbarEight"
+                            aria-controls="navbarEight"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <span class="toggler-icon" />
+                            <span class="toggler-icon" />
+                            <span class="toggler-icon" />
+                        </button>
 
-                      <div class="collapse navbar-collapse sub-menu-bar" id="navbarEight">
-                          <ul class="navbar-nav ml-auto">
-                              <li class="nav-item active">
-                                  <a class="page-scroll" href="#home">HOME</a>
-                              </li>
-                              <li class="nav-item">
-                                  <a class="page-scroll" href="#about">ABOUT</a>
-                              </li>
-                              <li class="nav-item">
-                                  <a class="page-scroll" href="#news">NEWS</a>
-                              </li>
-                              <li class="nav-item">
-                                  <a class="page-scroll" href="#partners">PARTNERS</a>
-                              </li>
-                              <li class="nav-item">
-                                  <a class="page-scroll" href="#testimonials">TESTIMONIALS</a>
-                              </li>
-                              <li class="nav-item">
-                                  <a class="page-scroll" href="#contact">CONTACT</a>
-                              </li>
-                          </ul>
-                      </div>
-                  </nav> <!-- navbar -->
-              </div>
-          </div> <!-- row -->
-      </div> <!-- container -->
-  </div> <!-- navbar area -->
+                        <div
+                            class="collapse navbar-collapse sub-menu-bar"
+                            id="navbarEight"
+                        >
+                            <ul class="navbar-nav ml-auto">
+                                {#each navbarItems as item, i (item.id)}
+                                    <li class="nav-item {item.active ? 'active' : ''}" >
+                                        <a class="page-scroll" href={item.href}>
+                                            {item.name}
+                                        </a>
+                                    </li>
+                                {/each}
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
 
-  <div id="home" class="slider-area">
-      <div class="bd-example">
-          <div id="carouselOne" class="carousel slide" data-ride="carousel">
-              <ol class="carousel-indicators">
-                  <li data-target="#carouselOne" data-slide-to="0" class="active"></li>
-                  <li data-target="#carouselOne" data-slide-to="1"></li>
-                  <li data-target="#carouselOne" data-slide-to="2"></li>
-              </ol>
+    <div id="home" class="slider-area">
+        <div class="bd-example">
+            <div id="carouselOne" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    {#each carouselItems as item, i (item.id)}
+                        <li
+                            data-target="#carouselOne"
+                            data-slide-to={i}
+                            class={i === 0 ? "active" : ""}
+                        />
+                    {/each}
+                </ol>
 
-              <div class="carousel-inner">
-                  <div class="carousel-item bg_cover active" style="background-image: url(assets/images/slider-1.jpg)">
-                      <div class="carousel-caption">
-                          <div class="container">
-                              <div class="row justify-content-center">
-                                  <div class="col-xl-6 col-lg-7 col-sm-10">
-                                      <h2 class="carousel-title">Empowering the future: Transforming education in Panama</h2>
-                                      <ul class="carousel-btn rounded-buttons">
-                                          <li><a class="main-btn rounded-three" href="#contact">Join Our Mission</a></li>
-                                          <!-- <li><a class="main-btn rounded-one" href="#">DOWNLOAD</a></li> -->
-                                      </ul>
-                                  </div>
-                              </div> <!-- row -->
-                          </div> <!-- container -->
-                      </div> <!-- carousel caption -->
-                  </div> <!-- carousel-item -->
+                <div class="carousel-inner">
+                    {#each carouselItems as item, i (item.id)}
+                        <div
+                            class="carousel-item bg_cover {i === 0 ? 'active' : ''}"
+                            style="background-image: url({item.backgroundImage})"
+                        >
+                            <div class="carousel-caption">
+                                <div class="container">
+                                    <div class="row justify-content-center">
+                                        <div class="col-xl-6 col-lg-7 col-sm-10" >
+                                            <h2 class="carousel-title">
+                                                {item.title}
+                                            </h2>
+                                            <ul class="carousel-btn rounded-buttons" >
+                                                <li>
+                                                    <a
+                                                        class="main-btn rounded-three"
+                                                        href={item.href}
+                                                    >
+                                                        {item.buttonText}
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    {/each}
+                </div>
 
-                  <div class="carousel-item bg_cover" style="background-image: url(assets/images/slider-2.jpg)">
-                      <div class="carousel-caption">
-                          <div class="container">
-                              <div class="row justify-content-center">
-                                  <div class="col-xl-6 col-lg-7 col-sm-10">
-                                      <h2 class="carousel-title">Unlocking potential: Digital tools for a brighter tomorrow</h2>
-                                      <ul class="carousel-btn rounded-buttons">
-                                          <li><a class="main-btn rounded-three" href="#contact">Get Involved</a></li>
-                                          <!-- <li><a class="main-btn rounded-one" href="#">DOWNLOAD</a></li> -->
-                                      </ul>
-                                  </div>
-                              </div> <!-- row -->
-                          </div> <!-- container -->
-                      </div> <!-- carousel caption -->
-                  </div> <!-- carousel-item -->
+                <a
+                    class="carousel-control-prev"
+                    href="#carouselOne"
+                    role="button"
+                    data-slide="prev"
+                >
+                    <i class="lni-arrow-left-circle" />
+                </a>
 
-                  <div class="carousel-item bg_cover" style="background-image: url(assets/images/slider-3.jpg)">
-                      <div class="carousel-caption">
-                          <div class="container">
-                              <div class="row justify-content-center">
-                                  <div class="col-xl-6 col-lg-7 col-sm-10">
-                                      <h2 class="carousel-title">Bridging the gap: Quality education for all Panamanian students</h2>
-                                      <ul class="carousel-btn rounded-buttons">
-                                          <li><a class="main-btn rounded-three" href="#contact">Transform Lives with Us</a></li>
-                                          <!-- <li><a class="main-btn rounded-one" href="#">DOWNLOAD</a></li> -->
-                                      </ul>
-                                  </div>
-                              </div> <!-- row -->
-                          </div> <!-- container -->
-                      </div> <!-- carousel caption -->
-                  </div> <!-- carousel-item -->
-              </div> <!-- carousel-inner -->
-
-              <a class="carousel-control-prev" href="#carouselOne" role="button" data-slide="prev">
-                  <i class="lni-arrow-left-circle"></i>
-              </a>
-
-              <a class="carousel-control-next" href="#carouselOne" role="button" data-slide="next">
-                  <i class="lni-arrow-right-circle"></i>
-              </a>
-          </div> <!-- carousel -->
-      </div> <!-- bd-example -->
-  </div>
-
+                <a
+                    class="carousel-control-next"
+                    href="#carouselOne"
+                    role="button"
+                    data-slide="next"
+                >
+                    <i class="lni-arrow-right-circle" />
+                </a>
+            </div>
+        </div>
+    </div>
 </section>
-
-<style></style>
