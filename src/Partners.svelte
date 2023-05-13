8fc1f83bf93ca1a8bc491fea0196e7464138d9da
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2023-05-12 21:10:48 -0500

    refactor(Partners): migrate the data to data.json and use the data as variables in Svelte component

--- src/Partners.svelte
+++ src/Partners.svelte
@@ -1,53 +1,28 @@
-<script></script>
+<script>
+    import data from './data.json';
+    let { title, description, images } = data.partners;
+</script>
 
 <section id="partners" class="partners-area">
-  <div class="container">
-      <div class="row justify-content-center">
-          <div class="col-lg-6">
-              <div class="section-title text-center pb-20">
-                  <h3 class="title">Partners</h3>
-                  <p class="text">This project is made possible through the concerted efforts of our committed partners. These include Rotary clubs, community organizations, and the Ministry of Education, all working together to reshape the educational landscape in Panama.</p>
-              </div> <!-- section title -->
-          </div>
-      </div> <!-- row -->
-      <div class="row client-active">
-          <div class="col-lg-3">
-              <div class="single-client text-center">
-                  <img src="assets/images/rotary-chitre.png" alt="Logo Rotary Club of Chitré">
-              </div> <!-- single client -->
-          </div>
-          <div class="col-lg-3">
-              <div class="single-client text-center">
-                  <img src="assets/images/rotary-cristobal-colon.png" alt="Logo Rotary Club of Cristobal Colón">
-              </div> <!-- single client -->
-          </div>
-          <div class="col-lg-3">
-              <div class="single-client text-center">
-                  <img src="assets/images/rotary-david.png" alt="Logo Rotary Club of David">
-              </div> <!-- single client -->
-          </div>
-          <div class="col-lg-3">
-              <div class="single-client text-center">
-                  <img src="assets/images/rotary-la-chorrera.png" alt="Logo Rotary Club of La Chorrera">
-              </div> <!-- single client -->
-          </div>
-          <div class="col-lg-3">
-              <div class="single-client text-center">
-                  <img src="assets/images/rotary-panama-norte.png" alt="Logo Rotary Club of Panama Norte">
-              </div> <!-- single client -->
-          </div>
-          <div class="col-lg-3">
-              <div class="single-client text-center">
-                  <img src="assets/images/rotary-playa-coronado.png" alt="Logo Rotary Club of Playa Coronado">
-              </div> <!-- single client -->
-          </div>
-          <div class="col-lg-3">
-              <div class="single-client text-center">
-                  <img src="assets/images/rotary-santiago.png" alt="Logo Rotary Club of Santiago">
-              </div> <!-- single client -->
-          </div>
-      </div> <!-- row -->
-  </div> <!-- container -->
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+                <div class="section-title text-center pb-20">
+                    <h3 class="title">{title}</h3>
+                    <p class="text">{description}</p>
+                </div> <!-- section title -->
+            </div>
+        </div> <!-- row -->
+        <div class="row client-active">
+            {#each images as image}
+            <div class="col-lg-3">
+                <div class="single-client text-center">
+                    <img src={image.src} alt={image.alt}>
+                </div> <!-- single client -->
+            </div>
+            {/each}
+        </div> <!-- row -->
+    </div> <!-- container -->
 </section>
 
 <style></style>
