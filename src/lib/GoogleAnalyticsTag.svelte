e3427d68818a2b259798eaec7d8afc7f425c3d8d
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2023-05-26 15:50:53 -0500

    feat: add Google analytics

---
+++ src/lib/GoogleAnalyticsTag.svelte
@@ -0,0 +1,2 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-MKKD11XZTH"></script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import App from './App.svelte'
+import App from './App.svelte';
+import './lib/GoogleAnalyticsTag.svelte';
+import './lib/GoogleAnalytics.svelte';
 
 const app = new App({
   target: document.getElementById('app'),
 })
 
-export default app
+export default app;
