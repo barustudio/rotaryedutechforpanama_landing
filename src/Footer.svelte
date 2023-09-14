b2bd8fa903bdaf9034e3081c1fe0f798f22fa254
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2023-09-14 15:49:47 -0500

    feat(Footer): add developer info and copyright

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,11 +1,26 @@
+<script>
+  const year = new Date().getFullYear();
+</script>
+
 <footer id="footer" class="footer-area">
   <div class="footer-copyright">
     <div class="container">
       <div class="row align-items-center">
-        <div class="col-lg-10">
+        <div class="col-10">
           <div class="copyright text-center text-lg-left mt-10">
             <p class="text">
-              Crafted by <a
+              Developed with 💚 in 🇵🇦
+              <span class="separator">|</span>
+              &copy;&nbsp;
+              <a
+                style="color: #17458f"
+                rel="nofollow"
+                href="https://barustudio.com"
+                target="_blank"
+              >BaruStudio</a>  { year }
+            </p>
+            <small>
+              Template by <a
                 style="color: #17458f"
                 rel="nofollow"
                 href="https://uideck.con">UIdeck</a
@@ -22,15 +37,16 @@
                 title="association icons">
                 Freepik - Flaticon
               </a>
-            </p>
+            </small>
           </div>
         </div>
-        <div class="col-lg-2">
+        <div class="col-2">
           <div class="footer-logo text-right mt-10">
-            <a href="index.html">
+            <a href="https://barustudio.com">
               <img
-                src="images/favicon.png"
-                alt="Logo of Digital Interactive Classrooms Project, a Rotary initiative."
+                width="48"
+                src="images/barustudio-64.png"
+                alt="BaruStudio Logo"
               />
             </a>
           </div>
@@ -39,3 +55,14 @@
     </div>
   </div>
 </footer>
+
+<style>
+  small {
+    font-size: 70%;
+    line-height: 1;
+  }
+  .separator {
+    color: rgb(0, 163, 0);
+    margin: 0 1rem;
+  }
+</style>
