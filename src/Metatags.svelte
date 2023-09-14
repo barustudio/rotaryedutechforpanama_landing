eff35c3a0ab2621d9be73a63f056231e9bd70ce5
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2023-09-14 15:27:03 -0500

    chore(Metatags): update the meta and include a page title

--- src/Metatags.svelte
+++ src/Metatags.svelte
@@ -3,20 +3,36 @@
 </script>
 
 <MetaTags
+  title="Digital Interactive Classrooms Project"
+  description="Empowering the future: Transforming education in Panama. A Rotary initiative."
+  canonical="https://rotaryedutechforpanama.org/"
   openGraph={{
+    url: 'https://rotaryedutechforpanama.org/',
+    title: 'Digital Interactive Classrooms Project',
+    description: 'Empowering the future: Transforming education in Panama. A Rotary initiative.',
     images: [
       {
-        url: '/images/slider-1.jpg',
+        url: 'images/slider-1.jpg',
         width: 1024,
         height: 768,
         alt: 'Empowering the future: Transforming education in Panama. A Rotary initiative.'
       },
       {
-        url: '/images/slider-1.jpg',
+        url: 'images/slider-1.jpg',
         width: 1024,
         height: 768,
         alt: 'Unlocking potential: Digital tools for a brighter tomorrow. A Rotary initiative.'
       }
-    ]
+    ],
+    siteName: 'Digital Interactive Classrooms Project'
+  }}
+  twitter={{
+    handle: '@handle',
+    site: '@site',
+    cardType: 'summary_large_image',
+    title: 'Digital Interactive Classrooms Project',
+    description: 'Empowering the future: Transforming education in Panama. A Rotary initiative.',
+    image: 'images/slider-1.jpg',
+    imageAlt: 'Empowering the future: Transforming education in Panama. A Rotary initiative.'
   }}
 />
